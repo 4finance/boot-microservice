de650e6befdc33353c82cf0a2e5375866ff5e4b7
szimano <szimano@szimano.org>
2014-06-20 12:36:50 +0200

    move microservice dependency management into separate jar hosted on https://github.com/4finance/micro-deps

--- src/main/groovy/com/ofg/infrastructure/discovery/DependencyNotDefinedException.groovy
+++
@@ -1,9 +0,0 @@
-package com.ofg.infrastructure.discovery
-
-import groovy.transform.InheritConstructors
-import groovy.transform.TypeChecked
-
-@TypeChecked
-@InheritConstructors
-class DependencyNotDefinedException extends RuntimeException {
-}
--- src/main/groovy/com/ofg/infrastructure/discovery/DependencyWatcherListener.groovy
+++
@@ -1,7 +0,0 @@
-package com.ofg.infrastructure.discovery
-
-interface DependencyWatcherListener {
-
-    void stateChanged(String dependencyName, DependencyState newState)
-
-}
