0e9dfe77a5cce2f1b1ee211a28ee803eb7fd51d2
Marcin Grzejszczak <marcin@grzejszczak.pl>
2014-08-04 17:36:53 +0200

    Added Swagger to controller

--- src/main/groovy/com/ofg/twitter/controller/PairIdController.groovy
+++ src/main/groovy/com/ofg/twitter/controller/PairIdController.groovy
@@ -1,5 +1,7 @@
 package com.ofg.twitter.controller
 import com.ofg.twitter.controller.place.extractor.PropagationWorker
+import com.wordnik.swagger.annotations.Api
+import com.wordnik.swagger.annotations.ApiOperation
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,15 +15,19 @@ import static org.springframework.web.bind.annotation.RequestMethod.PUT
 
 @Slf4j
 @RestController
+@Api(value = "pairId", description = "Collects places from tweets and propagates them to Collerators")
 class PairIdController {
 
-    @Autowired private PropagationWorker propagationWorker        
+    @Autowired private PropagationWorker propagationWorker
+
     
     @RequestMapping(
             value = '{pairId}',
             method = PUT,
             consumes = 'application/vnd.com.ofg.twitter-places-analyzer.v1+json',
             produces = 'application/vnd.com.ofg.twitter-places-analyzer.v1+json')
+    @ApiOperation(value = "Async collecting and propagating of tweets for a given pairId",
+            notes = "This will asynchronously call tweet collecting, place extracting and their propagation to Collerators")
     void getPlacesFromTweets(@PathVariable @NotNull long pairId, @RequestBody @NotNull String tweets) {
         propagationWorker.collectAndPropagate(pairId, tweets)
     }
