0a8dfb03fc41be669f92709770cf8432754c4244
mchmielarz <michal.chmielarz@gmail.com>
2014-09-16 12:46:01 +0200

    Exclusion on metrics configuration added to application's configuration.

--- src/main/groovy/com/ofg/microservice/Application.groovy
+++ src/main/groovy/com/ofg/microservice/Application.groovy
@@ -3,6 +3,7 @@ package com.ofg.microservice
 import com.ofg.infrastructure.environment.EnvironmentSetupVerifier
 import groovy.transform.TypeChecked
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.ComponentScan
@@ -14,7 +15,7 @@ import static com.ofg.config.BasicProfiles.*
 
 @TypeChecked
 @Configuration
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = MetricFilterAutoConfiguration)
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @ComponentScan(basePackages = ["com.ofg.microservice", "com.ofg.twitter"])
 @EnableCaching
