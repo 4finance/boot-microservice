81fd9e814ebfd369a02e6138923a96e04b5ce763
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2015-01-13 08:43:21 +0200

    Removed unnecessary disabling of jackson mappers fail on missing property

--- src/main/groovy/com/ofg/twitter/Application.groovy
+++ src/main/groovy/com/ofg/twitter/Application.groovy
@@ -1,15 +1,11 @@
 package com.ofg.twitter
 
-import com.fasterxml.jackson.databind.DeserializationFeature
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.ofg.infrastructure.environment.EnvironmentSetupVerifier
 import groovy.transform.TypeChecked
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.scheduling.annotation.EnableAsync
 
 import static com.ofg.config.BasicProfiles.*
@@ -27,12 +23,4 @@ class Application {
         application.run(args)
     }
 
-    @Bean
-    public MappingJackson2HttpMessageConverter jacksonConvertor(){
-        MappingJackson2HttpMessageConverter convertor= new MappingJackson2HttpMessageConverter();
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        convertor.setObjectMapper(mapper);
-        return convertor;
-    }
 }
