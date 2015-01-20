88bc67e3f65dc8a406d17b4f14d56b5cf7bcce59
Mariusz Smykuła <mariuszs@gmail.com>
2015-01-20 09:57:09 +0100

    time constant

--- src/main/groovy/com/ofg/twitter/config/CachingConfiguration.groovy
+++ src/main/groovy/com/ofg/twitter/config/CachingConfiguration.groovy
@@ -6,6 +6,10 @@ import org.springframework.cache.ehcache.EhCacheCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+import java.util.concurrent.TimeUnit
+
+import static java.util.concurrent.TimeUnit.MINUTES
+
 @Configuration
 class CachingConfiguration {
 
@@ -15,7 +19,7 @@ class CachingConfiguration {
         cacheConfiguration.setName("cities")
         cacheConfiguration.setMemoryStoreEvictionPolicy("LRU")
         cacheConfiguration.setMaxEntriesLocalHeap(1000)
-        cacheConfiguration.setTimeToLiveSeconds(60 * 10)
+        cacheConfiguration.setTimeToLiveSeconds(MINUTES.toSeconds(12))
         net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration()
         config.addCache(cacheConfiguration)
         return new EhCacheCacheManager(net.sf.ehcache.CacheManager.newInstance(config))
