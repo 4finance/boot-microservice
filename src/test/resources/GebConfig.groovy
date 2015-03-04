f2084ec19da8161be361f75e8df243149c4f0f98
Marcin Zajaczkowski <marcin.zajaczkowski@4finance.com>
2015-03-04 18:24:13 +0100

    [#58] GUI tests can be run on external application instance
    With -Dgeb.build.baseUrl="http://staging-env:9009"

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,7 +1,11 @@
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.firefox.FirefoxDriver
 
-baseUrl="http://localhost:8095"
+//baseUrl in this file has precedence over -Dgeb.build.baseUrl, so set it only if system property is not defined
+if (!System.getProperty("geb.build.baseUrl")) {
+    baseUrl="http://localhost:8095"
+}
+
 driver = {
     def firefoxDriver = new FirefoxDriver()
     firefoxDriver.manage().window().setSize(new Dimension(1600,1200))
