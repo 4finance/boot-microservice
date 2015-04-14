4f77ea5405b5bac8918fe1f508f86e5a384adadd
Ondrej Pospisil <Ondrej.Pospisil@4finance.com>
2015-04-14 15:16:13 +0200

    Delete doublequotes from set properties values

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,11 +1,11 @@
 rem ### Configuration which should be placed on server where app is deployed
 rem Environment where our app is deployed, configuration will be taken from corresponding dir
-@set APP_ENV="prod"
+@set APP_ENV=prod
 rem Git repository with configuration. Absolute path should be used.
-@set CONFIG_FOLDER="properties"
+@set CONFIG_FOLDER=properties
 rem For secretProd env, ENCRYPT_KEY is required.
 rem Use encrypt.key if you pass it as -D option when running java
-@set ENCRYPT_KEY="secretEncryptKey"
+@set ENCRYPT_KEY=secretEncryptKey
 rem ###
 
 pushd ..
