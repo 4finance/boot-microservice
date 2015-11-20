4febda210ebdc9789567fe75a816a72fef0cdd19
Marcin Grzejszczak <marcin.grzejszczak@gmail.com>
2015-11-20 20:38:13 +0100

    Migrated filters

--- src/test/groovy/com/ofg/base/MicroserviceMvcWiremockSpec.groovy
+++ src/test/groovy/com/ofg/base/MicroserviceMvcWiremockSpec.groovy
@@ -1,8 +1,12 @@
 package com.ofg.base
 
 import com.ofg.infrastructure.base.MvcWiremockIntegrationSpec
-import com.ofg.infrastructure.web.correlationid.CorrelationIdFilter
+import org.springframework.cloud.sleuth.instrument.web.TraceFilter
+import com.ofg.infrastructure.web.correlationid.HeadersSettingFilter
 import com.ofg.twitter.Application
+import org.springframework.beans.factory.annotation.Autowire
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.trace.Trace
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder
@@ -10,9 +14,11 @@ import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder
 @ContextConfiguration(classes = [Application], loader = SpringApplicationContextLoader)
 class MicroserviceMvcWiremockSpec extends MvcWiremockIntegrationSpec {
 
+    @Autowired Trace trace
+
     @Override
     protected void configureMockMvcBuilder(ConfigurableMockMvcBuilder mockMvcBuilder) {
         super.configureMockMvcBuilder(mockMvcBuilder)
-        mockMvcBuilder.addFilter(new CorrelationIdFilter())
+        mockMvcBuilder.addFilter(new HeadersSettingFilter(), new TraceFilter(trace))
     }
 }
