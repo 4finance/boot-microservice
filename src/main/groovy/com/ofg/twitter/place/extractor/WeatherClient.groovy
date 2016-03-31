05d8dcdea519f0c5b370ab687c06f03b7c669235
konrad.dobrzynski <konrad.dobrzynski@4finance.com>
2016-03-31 09:42:44 +0200

    New version of spring-cloud-deps

--- src/main/groovy/com/ofg/twitter/place/extractor/WeatherClient.groovy
+++ src/main/groovy/com/ofg/twitter/place/extractor/WeatherClient.groovy
@@ -3,7 +3,6 @@ package com.ofg.twitter.place.extractor
 import com.netflix.hystrix.HystrixCommand
 import com.netflix.hystrix.HystrixCommandGroupKey
 import com.netflix.hystrix.HystrixCommandKey
-import com.ofg.infrastructure.hystrix.CorrelatedCommand
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient
 import groovy.transform.CompileStatic
 
@@ -19,33 +18,23 @@ class WeatherClient {
     }
 
     String findCity(double latitude, double longitude) {
-        withHystrix('findCity') {
-            return serviceRestClient.forExternalService()
-                    .get()
-                    .onUrl(cityFindingServiceUrl + "?lat=${latitude}&lon=${longitude}")
-                    .anObject()
-                    .ofType(String)
-        }
+        return serviceRestClient.forExternalService()
+                .get()
+                .withCircuitBreaker(key('findCity'))
+                .onUrl(cityFindingServiceUrl + "?lat=${latitude}&lon=${longitude}")
+                .anObject()
+                .ofType(String)
     }
 
     String isCityExistent(String cityNameToCheck) {
-        withHystrix('isCityExistent') {
-            return serviceRestClient.forExternalService()
-                    .get()
-                    .onUrl("$cityFindingServiceUrl?q=$cityNameToCheck")
-                    .anObject()
-                    .ofType(String)
-        }
+        return serviceRestClient.forExternalService()
+                .get()
+                .withCircuitBreaker(key('isCityExistent'))
+                .onUrl("$cityFindingServiceUrl?q=$cityNameToCheck")
+                .anObject()
+                .ofType(String)
     }
 
-    private def withHystrix(String name, Closure block) {
-        return new CorrelatedCommand(key(name)) {
-            @Override
-            def doRun() {
-                return block.call()
-            }
-        }.execute()
-    }
 
     private def HystrixCommand.Setter key(String name) {
         return HystrixCommand.Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey(WeatherClient.class.getSimpleName()))
