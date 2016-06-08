401edcf74792e84b1fe5cb1a655cf8eb557d46f2
konrad.dobrzynski <konrad.dobrzynski@4finance.com>
2016-06-08 16:56:17 +0200

    Removing sampler -> will add to Micro-infra

--- src/main/groovy/com/ofg/twitter/Application.groovy
+++ src/main/groovy/com/ofg/twitter/Application.groovy
@@ -1,12 +1,11 @@
 package com.ofg.twitter
+
 import com.ofg.config.BasicProfiles
 import com.ofg.infrastructure.environment.EnvironmentSetupVerifier
 import groovy.transform.TypeChecked
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.scheduling.annotation.EnableAsync
 
@@ -22,10 +21,4 @@ class Application {
         application.addListeners(new EnvironmentSetupVerifier(BasicProfiles.all()))
         application.run(args)
     }
-
-    @Bean
-    public AlwaysSampler defaultSampler() {
-        return new AlwaysSampler();
-    }
-
 }
