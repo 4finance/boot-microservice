f15473fd3aa847d8403a6e38951ecca18eb6f9fc
konrad.dobrzynski <konrad.dobrzynski@4finance.com>
2016-08-05 14:51:15 +0200

    Revert "remove deprecated microservice.json"
    This reverts commit f1f0bdcc9d0051c75e05a3328b525e92028881d5.

--- src/test/groovy/com/ofg/twitter/geb/springcloud/SpringCloudAcceptanceSwaggerUISpec.groovy
+++ src/test/groovy/com/ofg/twitter/geb/springcloud/SpringCloudAcceptanceSwaggerUISpec.groovy
@@ -3,6 +3,6 @@ package com.ofg.twitter.geb.springcloud
 import com.ofg.twitter.geb.AcceptanceSwaggerUISpec
 import org.springframework.boot.test.IntegrationTest
 
-@IntegrationTest("spring.profiles.active:dev")
+@IntegrationTest("spring.profiles.active:dev,springCloud")
 class SpringCloudAcceptanceSwaggerUISpec extends AcceptanceSwaggerUISpec {
 }
--- src/test/groovy/com/ofg/twitter/geb/springcloud/SpringCloudSwaggerHealthEndpointUISpec.groovy
+++ src/test/groovy/com/ofg/twitter/geb/springcloud/SpringCloudSwaggerHealthEndpointUISpec.groovy
@@ -3,7 +3,7 @@ package com.ofg.twitter.geb.springcloud
 import com.ofg.twitter.geb.SwaggerHealthEndpointUISpec
 import org.springframework.boot.test.IntegrationTest
 
-@IntegrationTest("spring.profiles.active:dev")
+@IntegrationTest("spring.profiles.active:dev,springCloud")
 class SpringCloudSwaggerHealthEndpointUISpec extends SwaggerHealthEndpointUISpec {
 
 }
